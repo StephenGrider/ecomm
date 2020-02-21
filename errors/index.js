473054ccfe8822e64d3744cf8c0ddc2b33e32970
stephen grider <ste.grider@gmail.com>
2020-02-21 15:21:26 -0700

    update 401 err

--- errors/index.js
+++ errors/index.js
@@ -1,13 +1,13 @@
 const BadRequest = require('./bad-request-error');
 const ValidationError = require('./validation-error');
 const RequestValidationError = require('./request-validation-error');
-const UnauthorizedError = require('./unauthorized-error');
+const NotAuthorizedError = require('./not-authorized-error');
 const NotFoundError = require('./not-found-error');
 
 module.exports = {
   BadRequest,
   ValidationError,
   RequestValidationError,
-  UnauthorizedError,
+  NotAuthorizedError,
   NotFoundError
 };
