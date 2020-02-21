b815b0652e45ba56c7200898a129182d1b2eed1f
stephen grider <ste.grider@gmail.com>
2020-02-21 14:42:24 -0700

    add publish error

---
+++ events/publish-error.js
@@ -0,0 +1,7 @@
+module.exports = class PublishValidationError extends Error {
+  constructor(validationError) {
+    super(`Invalid event data: \n${JSON.stringify(validationError, null, 2)}`);
+
+    this.data = validationError;
+  }
+};
